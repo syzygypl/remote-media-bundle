ba2a2af60fd10622baa5ecdab91581af097cc38c
FrostF0X <dmytro.konowalow@gmail.com>
2020-06-05 12:52:31 +0200

    support kunstman >=5.2

--- src/MediaHandler/Imagine/Cache/CacheManager.php
+++ src/MediaHandler/Imagine/Cache/CacheManager.php
@@ -2,12 +2,14 @@
 
 namespace ArsThanea\RemoteMediaBundle\MediaHandler\Imagine\Cache;
 
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 class CacheManager extends \Liip\ImagineBundle\Imagine\Cache\CacheManager
 {
     /**
      * @inheritDoc
      */
-    public function generateUrl($path, $filter, array $runtimeConfig = [], $resolver = null)
+    public function generateUrl($path, $filter, array $runtimeConfig = [], $resolver = null, $referenceType = UrlGeneratorInterface::ABSOLUTE_URL)
     {
         $path = parse_url($path, PHP_URL_PATH);
 
